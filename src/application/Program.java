67c88171eaeac27531df0752299a2aef8110b226
wesleylelo <wesleylelo0078@gmail.com>
2022-01-20 20:14:53 -0300

    Organizando

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,7 @@ public class Program {
      */
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("Funcionou");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
